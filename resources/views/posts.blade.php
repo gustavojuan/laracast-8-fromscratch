e5e8e849418b26e7bfb761b2dedbbe8152fd4bfa
Gustavo Juan <gustavojuandev@gmail.com>
2023-12-05 13:57:09 +0100

    Route and views form posts and post

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My first Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis deleniti dolorem ducimus error illo inventore ipsum, iusto labore maxime modi mollitia nemo nesciunt non reprehenderit sapiente sint suscipit totam?</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis deleniti dolorem ducimus error illo inventore ipsum, iusto labore maxime modi mollitia nemo nesciunt non reprehenderit sapiente sint suscipit totam?</p>
+    </article>
+    <article>
+        <h1><a href="/post">My third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis deleniti dolorem ducimus error illo inventore ipsum, iusto labore maxime modi mollitia nemo nesciunt non reprehenderit sapiente sint suscipit totam?</p>
+    </article>
+
+</body>
+</html>
