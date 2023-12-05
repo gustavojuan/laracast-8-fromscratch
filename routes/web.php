885b192abddeeb5085c4952be941146c9fd3da90
Gustavo Juan <gustavojuandev@gmail.com>
2023-12-05 14:32:07 +0100

    [LESSON] Route Wildcard Constraints

--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,18 @@ Route::get('/', function () {
 Route::get('/posts/{post}', function ($slug) {
     $path = __DIR__."/../resources/posts/{$slug}.html";
 
-
     if(! file_exists( $path )) {
         return redirect('/');
         // abort(404);
     }
 
     $post = file_get_contents($path);
+
     return view('post', [
         'post' => $post
     ]);
 
 
-});
+})->where('post','[a-zA-Z0-9_-]+');
 
 
