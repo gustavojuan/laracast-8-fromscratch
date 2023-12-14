db549f066f948da829b7a8d8f8c0e79658128140
Gustavo Juan <gustavojuandev@gmail.com>
2023-12-14 11:30:43 +0100

    [LESSON]Find a Composer Package for Post Metadata 2 COLLECTIONS

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
@@ -30,28 +31,23 @@ class Post
         $this->slug = $slug;
     }
 
-    /**
-     * @return array|string[]
-     */
+
     public static function all()
     {
-        $files =  File::files(resource_path("posts"));
-
-        return array_map(function ($file){
-            return $file->getContents();
-        }, $files);
+        return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
     }
 
-    /**
-     * @param $slug
-     * @return mixed
-     */
+
     public static function find($slug)
     {
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new  ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Models\Post;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,28 +15,12 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-
-    $files =  File::files(resource_path("posts"));
-    $posts = [];
-
-    foreach ($files as $file){
-        $document = YamlFrontMatter::parseFile($file);
-
-        $posts[] = new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        );
-    }
-
-    //ddd($posts);
-    return view('posts', ['posts' => $posts]);
+    return view('posts', ['posts' =>  Post::all()]);
 });
 
 
 Route::get('/posts/{post}', function ($slug) {
+
     return view('post', [
         'post' => Post::find($slug)
     ]);
