2e3d336ca80ad7000120827704bf80a51f8cb6e5
Gustavo Juan <gustavojuandev@gmail.com>
2024-02-13 09:00:46 +0100

    [LESSON]: Clockwork, and the N+1 Problem

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,27 +11,31 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
+| routes are loaded by the R outeServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('posts', ['posts' =>  Post::all()]);
+
+
+//    DB::listen(function ($query){
+//        //\Illuminate\Support\Facades\Log::info('goo');
+//        //logger($query->sql, $query->bindings);
+//    });
+    return view('posts',['posts'=>Post::with('category')->get()]);
 });
 
 // Route Model Binding
-//{post} => $post
 
 //Route::get('/posts/{post}', function (Post $post) {
 //        return view('post', compact('post'));
 //});
 
-
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', compact('post'));
 });
 
-Route::get('/posts/{category:slug}', function (Category $category) {
+Route::get('/categories/{category:slug}', function (Category $category) {
     return view('category', compact('category'));
 });
