cc1e357708647b9b2258611256d401974f1effe4
Gustavo Juan <gustavojuandev@gmail.com>
2024-02-21 13:02:31 +0100

    lesson: Convert the HTML and CSS to Blade

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('username')->unique();
             $table->string('name');
+            
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -27,6 +28,7 @@ return new class extends Migration
      * Reverse the migrations.
      */
     public function down(): void
+
     {
         Schema::dropIfExists('users');
     }
