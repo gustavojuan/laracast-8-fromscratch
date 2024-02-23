9f5438a27d1520a48bf6cb0894c6f5eea9ae76e4
Gustavo Juan <gustavojuandev@gmail.com>
2024-02-23 12:53:39 +0100

    lesson: A Small JavaScript Dropdown Detour

--- routes/web.php
+++ routes/web.php
@@ -18,31 +18,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
     return view('posts',[
-        'posts'=>Post::latest('published_at')->with(['category','author'])->get()
+        'posts'=>Post::latest('published_at')->with(['category','author'])->get(),
+        'categories'=> Category::all()
     ]);
 });
 
-// Route Model Binding
-
-//Route::get('/posts/{post}', function (Post $post) {
-//        return view('post', compact('post'));
-//});
-
 Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', compact('post'));
+    return view('post',[
+        'posts'=> Post::all(),
+        'categories'=> Category::all()
+    ]);
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
    return view('posts', [
-        'posts'=>$category->posts
+        'posts'=>$category->posts,
+        'currentCategory'=> $category,
+        'categories'=> Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts'=>$author->posts
+        'posts'=>$author->posts,
+        'categories'=> Category::all()
+
     ]);
 });
 
