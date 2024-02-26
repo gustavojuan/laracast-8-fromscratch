8d2ef5224c123db212b9614f3949effeac9a8c92
Gustavo Juan <gustavojuandev@gmail.com>
2024-02-26 11:55:58 +0100

    lesson: Author Filtering

--- routes/web.php
+++ routes/web.php
@@ -8,22 +8,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use PHPUnit\Metadata\PostCondition;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the R outeServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/',[PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts'=>$author->posts,
-    ]);
-});
-
